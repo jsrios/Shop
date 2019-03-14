473a8447404bf65912e6c0f8a24ec242ce6758b1
jsrios <42100810+jsrios@users.noreply.github.com>
2019-03-13 21:56:31 -0500

    Register New User

--- Shop.Web/Controllers/ProductsController.cs
+++ Shop.Web/Controllers/ProductsController.cs
@@ -7,9 +7,12 @@
     using Data;
     using Data.Entities;
     using Helpers;
+    using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.EntityFrameworkCore;
     using Shop.Web.Models;
+
+    [Authorize]
 
     public class ProductsController : Controller
     {
@@ -80,8 +83,8 @@
                 }
 
                 var product = this.ToProduct(view, path);
-                // TODO: Pending to change to: this.User.Identity.Name
-                product.User = await this.userHelper.GetUserByEmailAsync("jzuluaga55@gmail.com");
+               
+                product.User = await this.userHelper.GetUserByEmailAsync("this.User.Identity.Name");
                 await this.productRepository.CreateAsync(product);
                 return RedirectToAction(nameof(Index));
             }
@@ -169,8 +172,8 @@
                     }
 
                     var product = this.ToProduct(view, path);
-                    // TODO: Pending to change to: this.User.Identity.Name
-                    product.User = await this.userHelper.GetUserByEmailAsync("jzuluaga55@gmail.com");
+                    
+                    product.User = await this.userHelper.GetUserByEmailAsync(this.User.Identity.Name);
                     await this.productRepository.UpdateAsync(product);
                 }
                 catch (DbUpdateConcurrencyException)
